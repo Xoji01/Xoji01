61a96eae67905a0f0dde7323cbdeb120638e25e5
Xoji01 <135627056+Xoji01@users.noreply.github.com>
2023-09-13 22:57:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+                //1
+let ee = prompt ('what is your name?')
+if (ee.charAt().toLocaleLowerCase() =='a') {alert('ok')}
+else {alert('no')}
+                 //2
+let qq = prompt ('Как вас зовут?')
+if(qq.length %2===0) {
+    alert('ok четное')
+}
+else {alert('No')}
+                //3
+let ww = prompt ('Введите цифру')
+if(ww %2===0) {
+    alert(ww + ' Четное')
+}
+else {alert(ww + ' Нечетное')}
+
